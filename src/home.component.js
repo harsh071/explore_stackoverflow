539982f1b7001f638b4ab97ec85fcf90f00494ee
Harsh <harshpatel071@gmail.com>
2021-03-03 01:30:03 -0600

    Updated UI, completed call from Assignment

--- src/home.component.js
+++ src/home.component.js
@@ -12,6 +12,7 @@ const HomeContainer = styled.div`
     flex-direction: column;
     align-items: center;   
     width:100%;
+    overflow:hidden;
 `
 const ListContainer = styled.div`
     margin: 20px 100px 100px 100px;
@@ -44,23 +45,41 @@ const Title = styled.h1`
 
 function Home() {
     let [tagged, setTagged] = useState("")
+    let [remaining, setRemaining] = useState(-1)
     let [questions, setQuestions] = useState([]);
 
     const handleSearchInput = (input) => {
         setTagged(input.target.value)
     }
     const handleSearch = () => {
-        // axios.get(
-        //     `https://api.stackexchange.com/2.2/questions?pagesize=10&fromdate=1613865600&todate=1614556800&order=desc&sort=votes&tagged=${tagged}&site=stackoverflow&filter=!*PBQNA2D-Y)jggiwb8s1L)bS3FX1MjsI4j0vFNb7pCMhx39lT`
-        // )
-        //     .then((response)=> {
-        //             console.log(response)
-        //             setQuestions(response.data.items)
-        //         }
-        //     ).catch((error)=>console.log(error))
-        // setQuestions(
-        //
-        // )
+        let dateTo = moment().format('YYYY-MM-DD hh:mm:ss');
+        let dateFrom = moment().subtract(7, 'd').format('YYYY-MM-DD hh:mm:ss');
+
+        axios.get(
+            `https://api.stackexchange.com/2.2/questions?pagesize=10&fromdate=${dateFrom}&todate=${dateTo}&order=desc&sort=creation&tagged=${tagged}&site=stackoverflow&filter=!*PBQNA2D-Y)jggiwb8s1L)bS3FX1MjsI4j0vFNb7pCMhx39lT`
+        )
+            .then((response) => {
+                    let list_of_questions = response.data.items;
+                    console.log(list_of_questions)
+                    axios.get(
+                        `https://api.stackexchange.com/2.2/questions?pagesize=10&fromdate=${dateFrom}&todate=${dateTo}&order=desc&sort=votes&tagged=${tagged}&site=stackoverflow&filter=!*PBQNA2D-Y)jggiwb8s1L)bS3FX1MjsI4j0vFNb7pCMhx39lT`
+                    )
+                        .then((inner_response) => {
+                                list_of_questions = list_of_questions.concat(inner_response.data.items)
+                                list_of_questions.sort(
+                                    (a, b) => {
+                                        return b.creation_date - a.creation_date
+                                    }
+                                )
+                                console.log(response)
+                                setQuestions(list_of_questions)
+                                setRemaining(response.data.quota_remaining)
+                            }
+                        ).catch((error) => {window.alert("ERROR OCCURED RETRY..."); console.log(error)})
+                }
+            ).catch((error) => {window.alert("ERROR OCCURED RETRY..."); console.log(error)})
+
+
     }
 
     useEffect(() => {
@@ -71,15 +90,16 @@ function Home() {
         <HomeContainer>
             <Title>EXPLORE STACKOVERFLOW</Title>
             <SearchContainer>
-                <TextField style={{width: "500px"}} id="standard-basic" label="Search Tagged" value={tagged}
+                <TextField style={{width: "500px"}} id="standard-basic" label={`Search Tagged`} value={tagged}
                            onChange={handleSearchInput}/>
                 <Button onClick={handleSearch} style={{background: "black", color: "white", marginLeft: "10px"}}
                         variant="contained"
                 >Search</Button>
             </SearchContainer>
             <ListContainer>
+                {remaining !== -1 && <>{remaining}{" searches remaining"}</>}
                 {questions.map((question, index) => <div style={{width: "100%"}} key={index}>
-                        <ListItem question={question} index={index} title={question.title}>{"Hahah"}
+                        <ListItem question={question} index={index} title={question.title}>
                         </ListItem>
                     </div>
                 )
@@ -125,8 +145,26 @@ const TitleSection = styled.div`
     flex-direction: column;
 `
 const Answer = styled.div`
-    margin-left: 40px;
-    background: #ebebeb;
+    background: white;
+    padding-left:5px;
+    box-shadow: 0px 0px 3px black;
+    margin-left:15px;
+    :hover{
+        box-shadow: 0px 0px 10px black;
+    }
+`
+const AnswerComment = styled.div`
+    margin-left: 20px;
+    margin-top: 2px;
+    background: white;
+    box-shadow: 0px 0px 3px black;
+    padding: 5px;
+    :hover{
+        box-shadow: 0px 0px 10px red;
+    }
+`
+const QuestionComment = styled(AnswerComment)`
+    
 `
 const BoldDiv = styled.div`
     font-weight: bold;
@@ -154,7 +192,7 @@ function ListItem(props) {
     const handleShowComment = () => {
         setShowComment(!showComment)
     }
-    console.log(props)
+    // console.log(props)
     return (
         <ListItemDiv>
             <DetailsContainer>
@@ -171,60 +209,67 @@ function ListItem(props) {
                 </QuestionTitle>
                 <Collapse in={show}>
                     <div style={{textAlign: 'left'}}>
-                        {<InnerHTML html={props.question.body}/>}
+                       {props.question.body && <><InnerHTML html={props.question.body}/></>}
                         {props.question.answers && <>
-                            <>ANSWERS:</>
+                            <>ANSWERS ({props.question.answers.length}):</>
                             <Button style={{background: "black", color: "white", marginLeft: "10px"}}
                                     onClick={handleShowAnswer}>{!showAnswer ? "SHOW" : "HIDE"}</Button>
                             <Collapse in={showAnswer}>
-
                                 {props.question.answers.map((answer, i) => <Answer>
-                                        <InnerHTML html={answer.body}/>
-                                        <TitleSection style={{marginLeft: "auto"}}>
+                                        <TitleSection style={{marginLeft: "auto"}}>({i+1})
                                             <BoldDiv>{"Date Created: "}{moment.unix(answer.creation_date).format("DD-MM-YYYY")}</BoldDiv>
                                             <BoldDiv>{"Votes: "}{answer.score}</BoldDiv>
-                                        </TitleSection>
-                                        <Answer>{answer.comments ?
+                                        </TitleSection><InnerHTML html={answer.body}/>
+                                        <>{answer.comments ?
                                             <>
-                                                <BoldDiv>ANSWER COMMENTS:</BoldDiv>
-                                                {answer.comments.map((comments, i) => <Answer
-                                                        style={{background: "white", padding: "3px"}}>
-                                                        <InnerHTML html={comments.body}/>
+                                                <BoldDiv>COMMENTS:</BoldDiv>
+                                                {answer.comments.map((comments, i) => <AnswerComment>
+                                                        ({i+1})
                                                         <TitleSection style={{marginLeft: "auto"}}>
                                                             <BoldDiv>{"Date Created: "}{moment.unix(comments.creation_date).format("DD-MM-YYYY")}</BoldDiv>
                                                             <BoldDiv>{"Votes: "}{comments.score}</BoldDiv>
                                                         </TitleSection>
-                                                        <hr/>
-                                                    </Answer>
+                                                        <InnerHTML html={comments.body}/>
+                                                 </AnswerComment>
                                                 )}</>
-                                            : ""}</Answer>
-                                        <hr/>
+                                            : ""}</>
                                     </Answer>
                                 )
                                 }
                             </Collapse>
                         </>}
                         {props.question.comments && <>
-                            <>COMMENTS:</>
+                            <>COMMENTS ({props.question.comments.length}):</>
                             <Button
                                 style={{background: "black", color: "white", margin: "10px"}}
                                 onClick={handleShowComment}>{!showComment ? "SHOW" : "HIDE"}</Button>
                             <Collapse in={showComment}>
-                                {props.question.comments.map((comments, i) => <Answer>
-                                        <InnerHTML html={comments.body}/>
+                                {props.question.comments.map((comments, i) => <QuestionComment>
+                                        ({i+1})
                                         <TitleSection style={{marginLeft: "auto"}}>
                                             <BoldDiv>{"Date Created: "}{moment.unix(comments.creation_date).format("DD-MM-YYYY")}</BoldDiv>
                                             <BoldDiv>{"Votes: "}{comments.score}</BoldDiv>
                                         </TitleSection>
-                                        <hr/>
-                                    </Answer>
+                                        <InnerHTML html={comments.body}/>
+                                    </QuestionComment>
                                 )
                                 }
                             </Collapse>
                         </>}
                         {
-                            !props.question.answers && <>NO ANSWERS</>
+                            !props.question.answers && <BoldDiv>NO ANSWERS</BoldDiv>
                         }
+                        {
+                            !props.question.comments && <BoldDiv>NO COMMENTS</BoldDiv>
+                        }
+                        <Button variant="contained" style={{
+                            background: !show ? "#EF8236" : "black",
+                            boxShadow: "0px 0px 4px black",
+                            color: "white",
+                            height: "40px",
+                            width: "150px",
+                            margin: "4px"
+                        }} onClick={handleShow}>{show ? "HIDE" : "SHOW"} THREAD</Button>
                     </div>
                 </Collapse>
             </DetailsContainer>
